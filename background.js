3e836c15986ac22a3cd82f726f88b55640039406
Onteia <yeyroy@gmail.com>
2023-04-16 02:55:17 -0700

    file defs js file and also make background follow mouse

---
+++ background.js
@@ -0,0 +1,21 @@
+
+let canvas;
+
+
+	let bg_width = 0;
+	let bg_height = 0;
+
+	let bgImg = new Image();
+	bgImg.src = BACKGROUND_GIF;
+	bgImg.addEventListener("load", function() {
+		bg_width = this.naturalWidth;
+		bg_height = this.naturalHeight;
+	});
+
+	document.addEventListener("mousemove", (event) => {
+		let mouse_x = event.x - Math.floor(bg_width/2);
+		let mouse_y = event.y - Math.floor(bg_height/2);
+		console.log(bg_width + ", " + bg_height);
+		document.body.style.backgroundPosition = `${mouse_x}px ${mouse_y}px`;
+	});
+
---
+++ file_defs.js
@@ -0,0 +1,2 @@
+const FILE_DEFS = "file_defs.js";
+const BACKGROUND_GIF = "background.gif";
