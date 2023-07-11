4edb4defae47d0357fe0dda52319c469a7aaef53
Onteia <yeyroy@gmail.com>
2023-07-10 17:14:15 -0700

    embed youtube, twitter, and images, twitch doesnt work

--- js/icebergitem.js
+++ js/icebergitem.js
@@ -15,6 +15,7 @@ class IcebergItem {
 	}
 
 	click() {
+		IcebergItem.unload_media();
 		this.hide();
 		this.item.show();
 	}
@@ -28,8 +29,29 @@ class IcebergItem {
 
 	static unhide_all() {
 		IcebergItem.hidden_items.forEach(item => {
-			item.hidden = false
+			item.hidden = false;
 		});
+		IcebergItem.unload_media();
+	}
+
+	static unload_media() {
+		const yt = document.getElementById('ytplayer');
+		if(yt !== null) {
+			yt.src = "";
+			yt.style.height = '0vw';
+		}
+		const thumb = document.getElementById('thumbnail');
+		if(thumb !== null) thumb.src = "";
+		const twt = document.getElementById('twitter-widget-0');
+		if(twt !== null) {
+			twt.src = "";
+			twt.style.height = '0px';
+		}
+		const twitch = document.getElementById('twitchplayer');
+		if(twitch !== null) {
+			twitch.src = "";
+			twitch.style.height = '0px';
+		}
 	}
 
 	print() {
@@ -41,5 +63,16 @@ class IcebergItem {
 }
 
 // these are for testing purposes; generate icebergitems from csv instead
-let thing1 = new IcebergItem(new Item("sahyj_", "Promised an obscene amount of money to play contract during subathon, then called out Aspect for cheating (using HUD): \"You fuck !\"", 3, "https://i.imgur.com/E08hGYF.png"));
-//let thing2 = new IcebergItem(new Item("thing twoooo", null, null, null));
+let thumbnail = new IcebergItem(new Item("sahyj_", "Promised an obscene amount of money to play contract during subathon, then called out Aspect for cheating (using HUD): \"You fuck !\"", 3, "https://i.imgur.com/E08hGYF.png"));
+
+let yt = new IcebergItem(new Item("Neon Ninja Suit", "The Neon Ninja Suit is a suit introduced in HITMAN III as a reward for beating any contract in Tier 8 of the Elusive Target Arcade. The suit was notable for when it was used in maps like Sapienza's \"The Icon\" or Paris' \"The Showstopper\". The use of the suit in Aspecticor's stream has been cited in 204 cases of permanent blindness across the globe, and 1500+ partial eyesight loss in North America alone. The Canadian government has been heavily sanctioned by multiple countries for not immediately taking legal action against Aspecticor.", 1, "https://youtube.com/shorts/itdcLLrOQFM"));
+
+//let two = new IcebergItem(new Item("money ballstreams", "ahh the good old days", 3, "https://youtube.com/shorts/ISq_0dIJmAo"));
+
+let twt = new IcebergItem(new Item("aspecticor fan club at UCB", "yooo test description", 0, "https://twitter.com/punchingwaves/status/1619931644881965056"));
+let twt2 = new IcebergItem(new Item("Hitman Horse", "HITMAAAAAAAAAAAAAAAAAAN HORSEEEEEEEEEEEEE", 0, "https://twitter.com/Linkus7/status/1579278016613490689"));
+
+
+let twitch = new IcebergItem(new Item("ALTF4", "Not to be confused with Super Alt F4, the program that Aspecticor uses to close Hitman Freelancer whenever he fails (L bozo), ALTF4 is a rage-inducing game where a clumsy knight traverses traps. Aspecticor streamed this at the beginning of his career, and to this day has not completed it, recently giving the outburst \"The creator of ALTF4 hasn't even completed the game, okay???\" The background music, entitled \"Round and Round,\" is said to trigger his PTSD to this day.", 5, "https://clips.twitch.tv/BelovedHorribleRabbitRickroll-F1NFa8ayNG5Ri0Qz"));
+
+
--- js/item.js
+++ js/item.js
@@ -15,8 +15,22 @@ class Item {
 		title.textContent = this.title;
 		let description = document.getElementById('description');
 		description.textContent = this.description;
-		let img = document.getElementById('thumbnail');
-		img.src = this.url;
+		if(this.url.includes("youtu")) {
+			const player = document.getElementById('ytplayer');
+			player.src = "https://www.youtube-nocookie.com/embed/" + this.url.split('/')[4];
+			player.style.height = '18.56vw';
+		} else if(this.url.includes("twitter")) {
+			const player = document.getElementById('twitter-widget-0');
+			player.src = "https://platform.twitter.com/embed/Tweet.html?theme=dark&id=" + this.url.split('/')[5];
+		} else if(this.url.includes("twitch")) {
+			const player = document.getElementById('twitchplayer');
+			player.src = "https://clips.twitch.tv/embed?clip=" + this.url.split('/')[3] + "&parent=www.github.com&parent=www.onteia.github.io";
+			player.style.height = '18.56vw';
+		}
+		else {
+			let img = document.getElementById('thumbnail');
+			img.src = this.url;
+		}
 	}
 }
 
